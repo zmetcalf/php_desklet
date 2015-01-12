75e8b7f812fcba3e0b2259a6364989cc592c6662
Zachery Metcalf <zachery.metcalf@gmail.com>
2015-01-12 00:22:30 +0000

    Moving changes from c9

--- app/views/index.php
+++ app/views/index.php
@@ -14,28 +14,28 @@
 
   </head>
   <body ng-app="desklet_app">
-    <nav class="navbar navbar-default">
-      <div class="container-fluid">
-        <div class="navbar-header">
-          <a href="#message_widget/create" type="button" class="btn btn-default navbar-btn add-widget">Add Widget</a>
-          <a class="navbar-brand" href="/">PHP Desklet</a>
+    <div ng-controller="MainController">
+      <nav class="navbar navbar-default">
+        <div class="container-fluid">
+          <div class="navbar-header">
+            <button type="button" ng-click="add_widget('message_widget')" class="btn btn-default navbar-btn">Add Message Widget</button>
+            <a class="navbar-brand" href="/">PHP Desklet</a>
+          </div>
         </div>
+      </nav>
+      <div class="container">
+        <div class="widget-container col-md-12"></div>
       </div>
-    </nav>
-    <div class="container" ng-controller="MainController">
-      <div class="widget-container col-md-12"><div ng-view></div></div>
     </div>
 
     <!-- CDN JS -->
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jqueryui/1.11.2/jquery-ui.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.8/angular.js"></script>
-    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular.js/1.3.8/angular-route.js"></script>
     <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/angular-ui-bootstrap/0.12.0/ui-bootstrap.js"></script>
 
     <!-- Local JS -->
     <script type="text/javascript" src="static/app/app.module.js"></script>
-    <script type="text/javascript" src="static/app/app.route.js"></script>
     <script type="text/javascript" src="static/app/shared/main/main_controller.js"></script>
     <script type="text/javascript" src="static/app/components/message_widget/message_widget_directive.js"></script>
     <script type="text/javascript" src="static/app/components/message_widget/message_widget_controller.js"></script>
--- public/static/app/app.route.js
+++
@@ -1,6 +0,0 @@
-desklet_app.config(function($routeProvider) {
-  $routeProvider.when('/message_widget/create', {
-    templateUrl: 'static/app/components/message_widget/message_widget.html',
-    controller: 'MessageWidgetController'
-  });
-});
